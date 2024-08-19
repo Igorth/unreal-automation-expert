98614ba568155292d6f44f7fdd9b1d3d911dea23
igord <igordiasth@gmail.com>
2024-08-19 11:15:10 -0400

    add try except `power_of_two.py`

--- power_of_two.py
+++ power_of_two.py
@@ -13,16 +13,20 @@ not_powered = 0
 for asset in selected_assets:
     asset_name = asset.get_name()
     asset_path = asset.get_path_name()
-    x_size = asset.blueprint_get_size_x()
-    y_size = asset.blueprint_get_size_y()
 
-    # check if both values are power of two
-    is_x_valid = math.log(x_size, 2).is_integer()
-    is_y_valid = math.log(y_size, 2).is_integer()
+    try:
+        x_size = asset.blueprint_get_size_x()
+        y_size = asset.blueprint_get_size_y()
 
-    if not is_x_valid or not is_y_valid:
-        unreal.log(f"{asset_name} is not a power of two ({x_size}, {y_size})")
-        unreal.log(f"It's path is {asset_path}")
-        not_powered += 1
+        # check if both values are power of two
+        is_x_valid = math.log(x_size, 2).is_integer()
+        is_y_valid = math.log(y_size, 2).is_integer()
+
+        if not is_x_valid or not is_y_valid:
+            unreal.log(f"{asset_name} is not a power of two ({x_size}, {y_size})")
+            unreal.log(f"It's path is {asset_path}")
+            not_powered += 1
+    except Exception as err:
+        unreal.log(f"{asset_name} is not a Texture - {err}")
 
 unreal.log(f"{num_assets} checked, {not_powered} textures found problematic.")
