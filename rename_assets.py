291cd7a01df430a9c0ac153ff76c2d123b10403c
igord <igordiasth@gmail.com>
2024-08-19 10:08:00 -0400

    add case-sensitive check `rename_assets.py`

--- rename_assets.py
+++ rename_assets.py
@@ -1,7 +1,7 @@
 import unreal
 
 
-def rename_assets(search_pattern, replace_pattern):
+def rename_assets(search_pattern, replace_pattern, use_case):
     # Instances of Unreal classes
     system_lib = unreal.SystemLibrary()
     editor_util = unreal.EditorUtilityLibrary()
@@ -19,8 +19,9 @@ def rename_assets(search_pattern, replace_pattern):
         asset_name = system_lib.get_object_name(asset)
 
         # Check if the asset name contains a specific substring to be replaced
-        if string_lib.contains(asset_name, search_pattern, use_case=True):
-            replaced_name = string_lib.replace(asset_name, search_pattern, replace_pattern)
+        if string_lib.contains(asset_name, search_pattern, use_case=use_case):
+            search_case = unreal.SearchCase.CASE_SENSITIVE if use_case else unreal.SearchCase.IGNORE_CASE
+            replaced_name = string_lib.replace(asset_name, search_pattern, replace_pattern, search_case)
             editor_util.rename_asset(asset, replaced_name)
             replaced += 1
             unreal.log(f"Renamed '{asset_name}' to '{replaced_name}'")
@@ -30,5 +31,5 @@ def rename_assets(search_pattern, replace_pattern):
     unreal.log(f"Replaced {replaced} of {num_assets} assets")
 
 
-rename_assets("Material", "Mat01")
+rename_assets("material", "Mat", False)
 
